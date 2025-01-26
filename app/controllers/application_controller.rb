046726ea60d8accd54ff8cbcd9e56cad28eb4b0c
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-01-26 12:11:07 +0000

    [finish]4_6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+ #全てのコントローラに対する処理を行える権限を持つ、ApplicationControllerに記述する
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
