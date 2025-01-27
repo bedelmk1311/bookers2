ae5e5323373a1d59c0d1d8c5cc6b0762b0701a89
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-01-27 15:56:44 +0000

    [finish]4-15

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
-  def top
+  def show
+    @user = User.find(params[:id])
+     # データ（レコード）を1件取得
+    @books = @user.books
+    #特定のユーザ（@user）に関連付けられた投稿全て（.post_images）を取得,@post_imagesに渡す 
+
   end
+
+  def edit
+    #@book = Book.new
+    #@books = Book.all
+  end
+
 end
