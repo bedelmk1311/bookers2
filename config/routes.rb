30063a113117734f9f534ff1b31f73a1ce28d682
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-01-27 05:36:47 +0000

    [finish]4-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users #devise を使用する際に URL として users を含むことを示しています。
   root to: "homes#top"
-  resources :books, #only: [:new, :index, :show, :edit]
+  resources :post_images, only: [:new, :index, :show]
   get "/homes/about" => "homes#about", as: "about"
 end
