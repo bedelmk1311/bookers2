fcf29e75e7f377206b10a55abce9498c9963f412
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-02-01 09:27:09 +0000

    [finish]4-6

--- db/migrate/20250126104246_devise_create_users.rb
+++ db/migrate/20250126104246_devise_create_users.rb
@@ -4,7 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: "" #メールアドレス
+      t.string :name,              null: false, default: "" #メールアドレス→名前
       t.string :encrypted_password, null: false, default: "" # 暗号化されたパスワード
 
       ## Recoverable
