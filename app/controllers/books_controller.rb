70d181b22de98aabda7bc04f7d047ce47d3d3dc4
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-02-19 05:34:54 +0000

    [errer]0 books編集権限

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,7 @@
 class BooksController < ApplicationController
+
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def new
     @book = Book.new
   end
@@ -54,5 +57,13 @@ class BooksController < ApplicationController
    def book_params
      params.require(:book).permit(:title, :body)
    end
+
+   def is_matching_login_user
+    book = Book.find(params[:id])
+    user = book.user
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
+  end
 
 end
